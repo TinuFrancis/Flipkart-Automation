a12266bcb58e0d9c5df8419afd99d0b8ec43de2c
Tinu Francis <tinufrancis21@gmail.com>
2026-03-23 17:14:04 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -4,16 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.text.NumberFormat;
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.logging.Level;
-
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
@@ -22,17 +31,17 @@ public class TestCases {
     ChromeDriver driver;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -46,16 +55,89 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
     }
 
+    // Test to Search "Washing Machine". Sort by popularity and print the count of
+    // items with rating less than or equal to 4 stars.
+    @Test
+    public void testCase01() throws InterruptedException {
+        driver.get("https://www.flipkart.com/");
+        Wrappers wrappers = new Wrappers();
+        wrappers.checkPopupDisplayed(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        Boolean status = false;
+
+        status = wrappers.searchForItem(driver, "Washing Machine");
+        Assert.assertTrue(status);
+
+        status = wrappers.sortByPopularity(driver);
+        Thread.sleep(3000);
+        Assert.assertTrue(status);
+
+        int count = wrappers.checkRatingBelow(driver, 4.0);
+        System.out.println("The number of items with star rating less than 4 = " + count);
+    }
+
+    // Test to Search "iPhone", print the Titles and discount % of items with more
+    // than 17% discount
+    @Test
+    public void testCase02() {
+        driver.get("https://www.flipkart.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        Boolean status = false;
+        Wrappers wrappers = new Wrappers();
+        status = wrappers.searchForItem(driver, "iPhone");
+        Assert.assertTrue(status);
+
+        wrappers.checkDiscountAbove(driver, 17);
+    }
+
+    // //Search "Coffee Mug", select 4 stars and above, and print the Title and
+    // image URL of the 5 items with highest number of reviews
+    @Test
+    public void testCase03() throws InterruptedException {
+        driver.get("https://www.flipkart.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        Boolean status = false;
+        Wrappers wrappers = new Wrappers();
+        status = wrappers.searchForItem(driver, "Coffee Mug");
+        Assert.assertTrue(status);
+
+        status = wrappers.checkRatingAboveFour(driver);
+        Thread.sleep(2000);
+        Assert.assertTrue(status);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        List<WebElement> reviewsCounts = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@class='PvbNMB']")));
+        Thread.sleep(2000);
+        List<Integer> allReviewCounts = new ArrayList<>();
+        for (WebElement reviewCount : reviewsCounts) {
+            String reviewCountText = reviewCount.getText().replaceAll("[(),]", "");
+            allReviewCounts.add(Integer.parseInt(reviewCountText));
+        }
+        Collections.sort(allReviewCounts, Collections.reverseOrder());
+        for (int i = 0; i < 5; i++) {
+            NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
+            String formattedNum = numberFormat.format(allReviewCounts.get(i));
+            WebElement productElement = driver.findElement(
+                    By.xpath("//span[@class='PvbNMB' and text()='(" + formattedNum + ")']/preceding::a[1]"));
+            String title = productElement.getText();
+            String imageUrl = productElement.getAttribute("href");
+
+            System.out.println("Product: " + title + "and the url: " + imageUrl);
+        }
+    }
+
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -19,4 +20,125 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    public void checkPopupDisplayed(WebDriver driver){
+        WebDriverWait wdwait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement popup = wdwait.until(ExpectedConditions.visibilityOfElementLocated(By.className("qc0M7c")));
+        if(popup.isDisplayed()){
+            WebElement closeButton = driver.findElement(By.xpath("//span[@role='button' and @class='b3wTlE']"));
+            closeButton.click();
+        }
+    }
+
+
+    public boolean searchForItem(WebDriver driver, String item){
+        try{
+            WebElement searchBox = driver.findElement(By.xpath("(//input[@name='q'])[1]"));
+            searchBox.sendKeys(item);
+            searchBox.sendKeys(Keys.ENTER);
+            Thread.sleep(3000);
+
+            if(item.contains(" ")){
+                String firstString = item.substring(0, item.indexOf(' '));
+                String currentUrl = driver.getCurrentUrl();
+                if (currentUrl.contains(firstString)){
+                    return true;
+                }
+            }else{
+                String currentUrl = driver.getCurrentUrl();
+                if (currentUrl.contains(item)){
+                    return true;
+                }
+            }
+            return false;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean sortByPopularity(WebDriver driver) {
+        try{
+            WebDriverWait wdwait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            Thread.sleep(2000);
+            WebElement sortByTab = wdwait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[@class='WNv7PR' and text() ='Popularity']")));
+            Thread.sleep(2000);
+            sortByTab.click();
+            String currentUrl = driver.getCurrentUrl();
+            if (currentUrl.contains("sort=popularity")) {
+                return true;
+            }
+            return false;
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public int checkRatingBelow(WebDriver driver, double stars) {
+        try{
+            int count = 0;
+            WebDriverWait wdwait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            List<WebElement> starRatings = wdwait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='jIjQ8S']//div[@class='MKiFS6']")));
+            for (WebElement rating : starRatings) {
+                String star = rating.getText();
+                double numStar = Double.parseDouble(star);
+                if (numStar <= stars) {
+                    count += 1;
+                }
+            }
+            return count;
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("There is an exception");
+            return 0;
+        }
+    }
+
+    public void checkDiscountAbove(WebDriver driver, int perc) {
+        // List<String> discounts = new ArrayList<>();
+        int count = 0;
+        List<WebElement> discountPercs = driver.findElements(By.xpath("//div[@class='jIjQ8S']//div[@class='HQe8jr']"));
+        for (WebElement discountPerc : discountPercs) {
+            String discountText = discountPerc.getText();
+            int textLength = discountText.length();
+            String numOnly = "";
+            for (int i = 0; i < textLength; i++) {
+                if (discountText.charAt(i) == '%') {
+                    break;
+                } else {
+                    numOnly += discountText.charAt(i);
+                }
+            }
+            if (Integer.parseInt(numOnly) >= perc) {
+                count += 1;
+                WebElement parentElement = discountPerc.findElement(
+                        By.xpath("(.//div[@class='jIjQ8S']//div[@class='HQe8jr']/preceding::div[@class='jIjQ8S'])[1]"));
+                WebElement titleElement = parentElement.findElement(By.xpath(".//div[@class='RG5Slk']"));
+                WebElement discountPercentage = parentElement.findElement(By.xpath(".//div[@class='HQe8jr']"));
+
+                System.out.println("Title of product: " + titleElement.getText());
+                System.out.println("Title of product: " + discountPercentage.getText());
+            }
+        }
+        if (count == 0){
+            System.out.println("There are no discounts above " +perc);
+        }
+    }
+
+        public boolean checkRatingAboveFour(WebDriver driver) {
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement fourStarBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("((//div[@class='Za3X8s'])[1]//div[@class='buvtMR'])[1]")));
+            Actions actions = new Actions(driver);
+            actions.scrollToElement(fourStarBox);
+            fourStarBox.click();
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
